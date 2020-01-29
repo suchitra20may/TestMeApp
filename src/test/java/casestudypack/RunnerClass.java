0dd1d9d718d535a16868dbf87e8da406048c50f6
PDC3A-Training.PDC3A <PDC3A-Training.PDC3A@P3C-1PNMGP2.dir.svc.accenture.com>
2020-01-29 06:13:07 -0600

    done

--- src/test/java/casestudypack/RunnerClass.java
+++ src/test/java/casestudypack/RunnerClass.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "features", glue = { "casestudypack"},tags="@login")
+@CucumberOptions(features = "features", glue = { "casestudypack"},tags="@login",plugin = {"pretty","json:/target/cucumber.json"})
 public class RunnerClass {
 
 }
